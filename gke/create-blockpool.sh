2c76fd6327ca610e6b2a07b2eb74a18eb79dfab2
Timothy Harris <timothy.harris@eficode.com>
2020-01-27 19:38:15 +0100

    rook-ceph - move block pool creation to filesystem script. Add snapshot class.

--- gke/create-blockpool.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-kubectl create -f cephblock-storageclass.yaml
-kubectl create -f blockpool.yaml
--- gke/create-filesystem.sh
+++ gke/create-filesystem.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+kubectl create -f cephblock-storageclass.yaml
+kubectl create -f blockpool.yaml
+
+sleep 3
 
 kubectl create -f cephfs-storageclass.yaml
 kubectl create -f shared-fs.yaml
